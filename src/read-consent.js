08b51413339067514fa2e000a63a8daa0092a14c
Andrey Roth E <andreyroth@gmail.com>
2018-05-30 09:07:13 +0100

    updating(?) read-consent.js to ES5

--- src/read-consent.js
+++ src/read-consent.js
@@ -1,8 +1,8 @@
-const COOKIE_NAME = 'euconsent';
-const value = '; ' + document.cookie;
-const parts = value.split('; ' + COOKIE_NAME + '=');
-let consentStr = '';
+var COOKIE_NAME = 'euconsent';
+var value = '; ' + document.cookie;
+var parts = value.split('; ' + COOKIE_NAME + '=');
+var consentStr = '';
 if (parts.length === 2) {
 	consentStr = parts.pop().split(';').shift();
 }
-window.parent.postMessage({ vmReadConsent: { consentStr, consentData: {}} }, '*');
+window.parent.postMessage({ vmReadConsent: { consentStr: consentStr, consentData: {} } }, '*');
